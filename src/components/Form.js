4a2d3cff970214578052220336c26c42a2f205f7
Holly Henske <hcover333@gmail.com>
2021-08-12 16:07:05 -0500

    some confusion between home and app

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Form.css'
+
+const Form = () => {
+    
+    return console.log('form');
+       
+};
+
+export default Form;
---
+++ src/components/TimerPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TimerPage.css';
+
+const React = require('reacte');
+const ms = require('pretty-ms');
+
+
+export default TimerPage;
